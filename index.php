cb489354000b03b547c36e397ed7fe8e1dfc203c
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-15 13:53:27 +0200

    print hotels info to the page

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+foreach ($hotels as $hotel) {
+    $hotelName = $hotel['name'];
+    $hotelDesc = $hotel['description'];
+    $hotelPark = $hotel['parking'];
+    $hotelVote = $hotel['vote'];
+    $hotelDistToCenter = $hotel['distance_to_center'];
+    $hotelPark ? $parking = 'has parking' : $parking = 'has no parking';
+
+    echo "$hotelName ($hotelDesc): $hotelVote stars, $parking, is $hotelDistToCenter km from the center <br>";
+}
